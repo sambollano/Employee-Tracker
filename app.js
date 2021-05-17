2c44ca24106e8b8a02546ef1a620b00b104e0bb2
Samantha Arellano <Sambollano54@gmail.com>
2021-05-16 22:40:02 -0500

    .

---
+++ app.js
@@ -0,0 +1,144 @@
+const inquirer = require("inquirer")
+const mysql = require("mysql")
+const cTable = require('console.table');
+
+function startPrompt() {
+    inquirer.prompt([
+    {
+    type: "list",
+    message: "What would you like to do?",
+    name: "choice",
+    choices: [
+              "View All Employees?", 
+              "View All Employee's By Roles?",
+              "View all Emplyees By Deparments", 
+              "Update Employee",
+              "Add Employee?",
+              "Add Role?",
+              "Add Department?"
+            ]
+    }
+]).then(function(val) {
+        switch (val.choice) {
+            case "View All Employees?":
+              viewAllEmployees();
+            break;
+    
+          case "View All Employee's By Roles?":
+              viewAllRoles();
+            break;
+          case "View all Emplyees By Deparments":
+              viewAllDepartments();
+            break;
+          
+          case "Add Employee?":
+                addEmployee();
+              break;
+
+          case "Update Employee":
+                updateEmployee();
+              break;
+      
+            case "Add Role?":
+                addRole();
+              break;
+      
+            case "Add Department?":
+                addDepartment();
+              break;
+    
+            }
+    })
+}
+function startPrompt() {
+    inquirer.prompt([
+    {
+    type: "list",
+    message: "What would you like to do?",
+    name: "choice",
+    choices: [
+              "View All Employees?", 
+              "View All Employee's By Roles?",
+              "View all Emplyees By Deparments", 
+              "Update Employee",
+              "Add Employee?",
+              "Add Role?",
+              "Add Department?"
+            ]
+    }
+]).then(function(val) {
+        switch (val.choice) {
+            case "View All Employees?":
+              viewAllEmployees();
+            break;
+    
+          case "View All Employee's By Roles?":
+              viewAllRoles();
+            break;
+          case "View all Emplyees By Deparments":
+              viewAllDepartments();
+            break;
+          
+          case "Add Employee?":
+                addEmployee();
+              break;
+
+          case "Update Employee":
+                updateEmployee();
+              break;
+      
+            case "Add Role?":
+                addRole();
+              break;
+      
+            case "Add Department?":
+                addDepartment();
+              break;
+    
+            }
+    })
+}
+function addEmployee() { 
+    inquirer.prompt([
+        {
+          name: "firstname",
+          type: "input",
+          message: "Enter their first name "
+        },
+        {
+          name: "lastname",
+          type: "input",
+          message: "Enter their last name "
+        },
+        {
+          name: "role",
+          type: "list",
+          message: "What is their role? ",
+          choices: selectRole()
+        },
+        {
+            name: "choice",
+            type: "rawlist",
+            message: "Whats their managers name?",
+            choices: selectManager()
+        }
+    ]).then(function (val) {
+      var roleId = selectRole().indexOf(val.role)
+      var managerId = selectManager().indexOf(val.choice)
+      connection.query("INSERT INTO employee SET ?", 
+      {
+          first_name: val.firstName,
+          last_name: val.lastName,
+          manager_id: managerId,
+          role_id: roleId
+          
+      },
+      
+      function(err){
+          if (err) throw err
+          console.table(val)
+          startPrompt()
+      })
+
+  })
+}
--- schema.sql
+++ schema.sql
@@ -1,21 +1,49 @@
+--Employee Table--
+
+CREATE TABLE Employees (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  job_id INT,
+  FOREIGN KEY (job_id) REFERENCES job(id),
+  FOREIGN KEY (manager_id) REFERENCES Employees(id)
+
+);
+
+--Job Table--
+
+CREATE TABLE job (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(20),
+  salary DECIMAL,
+  branch_id INT,
+  FOREIGN KEY (branch_id) REFERENCES branch(id)
+);
+
+--Branch info--
+CREATE TABLE branch (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(20)
+ 
+);
 --EmployeeNames--
-INSERT INTO employees (first_name, last_name, manager_id, role_id)
+INSERT INTO Employees (first_name, last_name, manager_id, role_id)
 VALUE ("Sarah", "Carter", null, 1);
-INSERT INTO employees (first_name, last_name, manager_id, role_id)
+INSERT INTO Employees (first_name, last_name, manager_id, role_id)
 VALUE ("Luna", "Arellano", null, 2);
-INSERT INTO employees (first_name, last_name, manager_id, role_id)
+INSERT INTO Employees (first_name, last_name, manager_id, role_id)
 VALUE ("Taylor","Sheldon",null,3);
-INSERT INTO employees (first_name, last_name, manager_id, role_id)
+INSERT INTO Employees (first_name, last_name, manager_id, role_id)
 VALUE ("Bradley", "Smith", 1, 3);
-INSERT INTO employees (first_name, last_name, manager_id, role_id)
+INSERT INTO Employees (first_name, last_name, manager_id, role_id)
 VALUE ("Pierce", "Stewart", 1, 5);
-INSERT INTO employees (first_name, last_name, manager_id, role_id)
+INSERT INTO Employees (first_name, last_name, manager_id, role_id)
 VALUE ("Chelsea", "Swift", 1, 6);
-INSERT INTO employees (first_name, last_name, manager_id, role_id)
+INSERT INTO Employees (first_name, last_name, manager_id, role_id)
 VALUE ("Tom", "Cruz", 2, 7);
 
 
-(
 INSERT INTO branch (name)
 VALUE ("Operations"),
 INSERT INTO branch (name)
@@ -24,23 +52,22 @@ INSERT INTO branch (name)
 VALUES ("Marketing"),
 INSERT INTO branch (name)
 VALUE ("Production"),
-)
 
 INSERT INTO job (title, salary, department_id)
 VALUE ("Lead Engineer", 150000, 2);
 INSERT INTO job (title, salary, department_id)
-VALUE ("Legal Team Lead", 250000, 4);
+VALUE ("Business Operations", 410000, 4);
 INSERT INTO job (title, salary, department_id)
-VALUE ("Accountant", 125000, 3);
+VALUE ("Accountant Manager", 25000, 3);
 INSERT INTO job (title, salary, department_id)
-VALUE ("Sales Lead", 100000, 1);
+VALUE ("Sales Lead", 80000, 1 )
 INSERT INTO job (title, salary, department_id)
-VALUE ("Salesperson", 80000, 1);
+VALUE ("Lawyer", 100000, 1);
 INSERT INTO job (title, salary, department_id)
-VALUE ("Software Engineer", 120000, 2);
+VALUE ("Gamer", 120000, 2);
 INSERT INTO job (title, salary, department_id)
-VALUE ("Lawyer", 100000, 4);
+VALUE ("Cashier", 100000, 4);
 
-SELECT * FROM employee;
+SELECT * FROM Employees;
 SELECT * FROM branch;
 SELECT * FROM job;
