f137aa0c95433efd88080a61503678900b24926a
Samantha Arellano <Sambollano54@gmail.com>
2021-07-28 15:10:49 -0500

    Update seeds.sql

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -1,42 +1,30 @@
-DROP DATABASE IF EXISTS Employee;
-
-CREATE database Employee_Tracker;
-
 USE Employee_Tracker;
 
-INSERT INTO department (name)
-VALUES ("Sales");
-INSERT INTO department (name)
-VALUES ("Customer Service");
-INSERT INTO department (name)
-VALUES ("Human Resources");
-INSERT INTO department (name)
-VALUES ("Legal");
+INSERT INTO department (dept_name)
+VALUES ('Sales'),
+       ('Finance'),
+       ('Engineering'),
+       ('Legal'),
+       ('Management'),
+       ('Operations');
 
-INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Manager", 101000, 1);
-INSERT INTO role (title, salary, department_id)
-VALUES ("Accountant", 250000, 2);
-INSERT INTO role (title, salary, department_id)
-VALUES ("Software Engineer", 320000, 2);
-INSERT INTO role (title, salary, department_id)
-VALUES ("Developer", 425000, 3);
-INSERT INTO role (title, salary, department_id)
-VALUES ("Lawyer", 550000, 4);
+--Department insert
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Manager', 101000, 1),
+       ('Accountant', 250000, 2),
+       ('Software Engineer', 320000, 2),
+       ('Developer', 425000, 3),
+       ('Lawyer', 550000, 4),
+       ('Lead Sales', 95000, 2),
+       ('Technician', 45000, 6),
+       ('Mechanical Engineer', 65000, 7);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Halsey", "Yungblud", 1, 3);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Travis", "Barker", 2, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Machine", "Gun", 3, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Kelly", "Disturbed", 4, 3);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tool", "Nirvana", 5, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("The", "Weekend", 2, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Ellie", "Goulding", 4, 7);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Good", "Charlotte", 1010101, 2);
+VALUES ('Lipa', 'Dua', 1, 3),
+       ('Rodrigo', 'Olivia', 2, 1),
+       ('Holt', 'Olivia', 3, 4),
+       ('Swift', 'Taylor', 4, 3),
+       ('Bieber', 'Justin', 5, 9),
+       ('Gomez', 'Selena', 2, 5),
+       ('Jauregui', 'Lauren', 4, 7),
+       ('Cabello', 'Camila', 1010101, 2);
